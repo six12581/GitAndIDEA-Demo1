1597209bcffca6419b0d66bfbbc5583fc80899f5
ahui <zch12581@126.com>
2022-06-10 14:45:45 +0800

    First Commit

---
+++ src/main/java/org/example/demo/Demo.java
@@ -0,0 +1,12 @@
+package org.example.demo;
+
+/**
+ * @Author: A hui
+ * @Create: 2022-06-10 14:43
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world！");
+    }
+}
